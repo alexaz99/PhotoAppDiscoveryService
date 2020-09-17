ab5fa009f3f87dc2b05f05f33ffd773fe2dde880
Alex <azakotyansky@gmail.com>
2020-09-16 20:24:48 -0400

    Add eureka.client settings

--- src/main/java/com/alex/photoapp/discovery/PhotoAppDiscoveryServiceApplication.java
+++ src/main/java/com/alex/photoapp/discovery/PhotoAppDiscoveryServiceApplication.java
@@ -19,8 +19,15 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
  Spring Apps Examples:
  https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples
-
  * @EnableEurekaServer
+ *
+ *  1. First start Eureka Discovery server ( PhotoAppDiscoveryServiceApplication.java app)
+ *  2. Then starts PhotoAppApiUsersApplication application
+ *  3. Open a browser and type => http://localhost:8010 will see the Eureka Dashboard
+ *  4. You will see the application name users-ws
+ *     settings are dome in application.properties for PhotoAppApiUsersApplication application
+ *       spring.application.name=users-ws
+ *  5. Click on running application status
  */
 @SpringBootApplication
 @EnableEurekaServer
